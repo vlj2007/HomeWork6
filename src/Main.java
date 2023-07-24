300c0bfe5b81cd6a0049d7f4ad00c2bb153cb0a4
vlj2007 <vlj2007@yandex.ru>
2023-07-24 21:15:15 +0700

    I_have_finished_task7

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         task4();
         task5();
         task6();
+        task7();
     }
 
     public static void task1(){
@@ -45,6 +46,13 @@ public class Main {
     public static void task6(){
         System.out.println("Задача 6");
         for (int i = 7; i < 99; i = i + 7){
+            System.out.println(i);
+        }
+    }
+
+    public static void task7(){
+        System.out.println("Задача 7");
+        for (int i = 1; i < 513; i = i * 2){
             System.out.println(i);
         }
     }
