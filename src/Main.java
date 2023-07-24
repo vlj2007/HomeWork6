8d0385ef1401d60d3bda90f0514019fc96993c7b
vlj2007 <vlj2007@yandex.ru>
2023-07-24 20:42:44 +0700

    I_have_finished_task2

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,19 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
 
     public static void task1(){
         System.out.println("Задача 1");
         for(int i =1; i < 11; i++){
+            System.out.println("Итерация цикла " + i);
+        }
+    }
+
+    public static void task2(){
+        System.out.println("Задача 2");
+        for(int i =10; i > 0; i--){
             System.out.println("Итерация цикла " + i);
         }
     }
