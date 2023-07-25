8b2342e80744b092d9f385a0d4a3f1d2c84edc4d
vlj2007 <vlj2007@yandex.ru>
2023-07-25 21:40:50 +0700

    code_correction_all_tasks_Cделано_ДЗ

--- src/Main.java
+++ src/Main.java
@@ -69,7 +69,7 @@ public class Main {
         int savings = 29000;
         int total = 0;
         int toTheBankAtPercent = 0;
-        for (int i = 1; i < 13; i++ ){
+        for (int i = 1; i <= 12; i++ ){
             toTheBankAtPercent = total + total/12;
             total = total + savings;
             System.out.println("Месяц " + i + ", сумма накоплений равна " + toTheBankAtPercent + " рублей");
